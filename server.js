d28683c606923d42352306f998f045ad12d4caba
Clinton <53704258+LordeOfTheWing@users.noreply.github.com>
2021-07-09 13:35:55 +0300

    Modelling the tours with mongoose

--- server.js
+++ server.js
@@ -14,12 +14,43 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("DB connection successful!");
   });
 
+//creating the tour schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, "A tour must have a price"] },
+});
+
+//modelling the tour
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(`Error:`, err.message);
+  });
+
 console.log(process.env.NODE_ENV);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App is running on port: ${PORT}`);
